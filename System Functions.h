5b22a82665782f9b166a02f92011b934a9208c96
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2024-12-15 20:17:14 -0500

    replaced if statement with case in main loop

--- System Functions.h
+++ System Functions.h
@@ -27,7 +27,30 @@ void printValues()
  
 }
 
+void getUserInput() {
+    brightness = uart_getc(UART_ID);
+    mode = uart_getc(UART_ID);
+    r = uart_getc(UART_ID);
+    g = uart_getc(UART_ID);
+    b = uart_getc(UART_ID);    
+}
+
 void interrupt()
 {
     irq_flag = true;
+}
+
+void setup() {
+  stdio_init_all();
+  PIO pio = pio0;
+  int sm = 0;
+  uint offset = pio_add_program(pio, &ws2812_program);
+  ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, false);
+  gpio_set_dir(IRQ_PIN, GPIO_IN);
+  gpio_init(25);
+  gpio_set_dir(25, GPIO_OUT);
+  gpio_pull_down(IRQ_PIN);
+  uart_init(UART_ID, BAUD_RATE);
+  gpio_set_irq_enabled_with_callback(IRQ_PIN, GPIO_IRQ_EDGE_RISE, true, interrupt);
+  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);    
 }
